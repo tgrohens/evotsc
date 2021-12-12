498475a667703ecb77c5dd0d3d264b6c5bdebb97
Théotime Grohens <theotime.grohens@insa-lyon.fr>
2021-12-12 13:11:06 +0100

    [refactoring] Move some code from notebook to evotsc_plot.py

--- evotsc_plot.py
+++ evotsc_plot.py
@@ -120,7 +120,105 @@ def explain(indiv, sigma_A, sigma_B):
     print(f'  B genes:     {on_genes_B[2]} on, {off_genes_B[2]} off')
 
 
-def plot_genome_and_tsc(indiv, sigma, print_ids=False, name=None):
+def plot_genome(indiv, print_ids=False, name=None):
+
+    # Compute gene positions
+    gene_pos, genome_length = indiv.compute_gene_positions()
+
+    # Plot
+    pos_rect = [0.1, 0.1, 0.8, 0.8]
+    fig = plt.figure(figsize=(9,9), dpi=dpi)
+    ax = fig.add_axes(pos_rect)
+
+    rect_width = 0.04
+    rect_height = 0.1
+
+    ax.set_xlim(-1.5, 1.5)
+    ax.set_ylim(-1.5, 1.5)
+    circle = plt.Circle(xy=(0, 0), radius=1, linestyle='-', fill=False)
+    ax.add_patch(circle)
+    ax.set_axis_off()
+
+    gene_type_color = ['tab:blue', 'tab:red', 'tab:green'] # AB, A, B
+
+    ## Plot the genes themselves
+    for i_gene, gene in enumerate(indiv.genes):
+        pos_angle = 360 * gene_pos[i_gene] / genome_length
+        orient_angle = 360 - pos_angle
+        pos_rad = np.radians(pos_angle)
+
+        ## Plot the gene rectangle
+
+        x0 = (1.0 - rect_height / 2.0) * np.sin(pos_rad)
+        y0 = (1.0 - rect_height / 2.0) * np.cos(pos_rad)
+
+
+        if gene.orientation == 0:
+            final_width = rect_width
+        else:
+            final_width = -rect_width
+
+
+        rect = plt.Rectangle(xy=(x0, y0),
+                             width=final_width,
+                             height=rect_height,
+                             angle=orient_angle, #in degrees anti-clockwise about xy.
+                             facecolor=gene_type_color[gene.gene_type],
+                             edgecolor='black',
+                             label=f'Gene {i_gene}')
+
+        ax.add_patch(rect)
+
+        ## Plot the orientation bar and arrow
+
+        # Bar
+        x_lin = (1.0 + (np.array([0.5, 1.0])) * rect_height) * np.sin(pos_rad)
+        y_lin = (1.0 + (np.array([0.5, 1.0])) * rect_height) * np.cos(pos_rad)
+
+        ax.plot(x_lin, y_lin, color='black', linewidth=1)
+
+        # Arrow
+        dx_arr = rect_width * np.cos(pos_rad) / 3.0
+        dy_arr = - rect_width * np.sin(pos_rad) / 3.0
+
+        if gene.orientation == 1: # Reverse
+            dx_arr, dy_arr = -dx_arr, -dy_arr
+
+        ax.arrow(x_lin[1], y_lin[1], dx_arr, dy_arr, head_width=0.02, color='black')
+
+        ## Print gene ID
+        if print_ids and (i_gene % 5 == 0):
+            ha = 'left'
+            if gene.orientation == 1:
+                ha = 'right'
+            ax.text(x=0.92*x0, y=0.92*y0, s=f'{i_gene}', rotation=orient_angle, ha=ha, va='bottom',
+                     rotation_mode='anchor')
+
+    ## Plot the legend
+    patches = [mpl.patches.Patch(facecolor=color, edgecolor='black', label=label)
+               for color, label in zip(gene_type_color, gene_types)]
+    ax.legend(handles=patches, title='Gene type', loc='center')
+
+    line_len = np.pi*indiv.interaction_dist/genome_length
+    line_y = -0.3
+    ax.plot([-line_len, line_len], [line_y, line_y],
+             color='black',
+             linewidth=1)
+    ax.text(0, line_y - 0.07, 'Gene interaction distance', ha='center')
+
+    if name:
+        plt.savefig(name, dpi=300, bbox_inches='tight')
+
+    plt.show()
+
+    plt.close()
+
+
+def plot_genome_and_tsc(indiv,
+                        sigma,
+                        show_bar=False,
+                        print_ids=False,
+                        name=None):
 
     # Compute gene positions
     gene_pos, genome_length = indiv.compute_gene_positions()
@@ -135,6 +233,7 @@ def plot_genome_and_tsc(indiv, sigma, print_ids=False, name=None):
 
     ax.set_xlim(-1.2, 1.2)
     ax.set_ylim(-1.2, 1.2)
+    ax.set_aspect('equal')
     circle = plt.Circle(xy=(0, 0), radius=1, linestyle='-', fill=False)
     ax.add_patch(circle)
     ax.set_axis_off()
@@ -142,13 +241,11 @@ def plot_genome_and_tsc(indiv, sigma, print_ids=False, name=None):
     ## Plot the genes themselves
 
     gene_type_color = ['tab:blue', 'tab:red', 'tab:green'] # AB, A, B
-    gene_types = ['AB', 'A', 'B']
 
     for i_gene, gene in enumerate(indiv.genes):
         pos_angle = 360 * gene_pos[i_gene] / genome_length
         orient_angle = 360 - pos_angle
         pos_rad = np.radians(pos_angle)
-        orient_rad = np.radians(orient_angle)
 
         ## Plot the gene rectangle
 
@@ -218,20 +315,23 @@ def plot_genome_and_tsc(indiv, sigma, print_ids=False, name=None):
     norm = mpl.colors.Normalize(-2.0, 2.0) # Extremum values for the SC level
 
     data = -data # Reverse data to get blue = positive and red = negative SC
-    sc_ax.pcolormesh(theta, radius, [data, data], shading='gouraud',
-                     norm=norm, cmap=plt.get_cmap('seismic'))
+
+    mesh = sc_ax.pcolormesh(theta, radius, [data, data], shading='gouraud',
+                            norm=norm, cmap=plt.get_cmap('seismic'))
     sc_ax.set_yticklabels([])
     sc_ax.set_xticklabels([])
     #sc_ax.spines['polar'].set_visible(False)
     sc_ax.set_theta_zero_location('N')
     sc_ax.set_theta_direction('clockwise')
 
-    ## Plot the legend
-    patches = [mpl.patches.Patch(facecolor=color, edgecolor='black', label=label)
-               for color, label in zip(gene_type_color, gene_types)]
-    ax.legend(handles=patches, title='Gene type', loc='center',
-              fontsize=15, title_fontsize=15)
+    # Color bar for the SC level
+    if show_bar:
+        cbar = fig.colorbar(mesh, ax=[ax, sc_ax], shrink=0.7, pad=0.0, location='left')
+        cbar.set_label('σ', fontsize=20)
+        cbar.ax.invert_yaxis()
+        cbar.ax.tick_params(labelsize=15)
 
+    ## Legend: the gene interaction distance
     line_len = np.pi*indiv.interaction_dist/genome_length
     line_y = -0.3
     ax.plot([-line_len, line_len], [line_y, line_y],
@@ -239,6 +339,7 @@ def plot_genome_and_tsc(indiv, sigma, print_ids=False, name=None):
              linewidth=1)
     ax.text(0, line_y - 0.07, 'Gene interaction distance', ha='center', fontsize=15)
 
+    ## Wrapping up
     if name:
         plt.savefig(name, dpi=300, bbox_inches='tight')
 
